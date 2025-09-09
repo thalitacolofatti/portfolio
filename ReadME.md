589e441487c7aff2497dc07b3c1bad8090090e64
Thalita Colofatti <62973671+thalitacolofatti@users.noreply.github.com>
2025-09-09 16:48:07 -0300

    Update responsive config and js file and add videos and prototypes

---
+++ ReadME.md
@@ -0,0 +1,5 @@
+# Projeto de Portfólio
+
+em construção
+
+Archviz foi arquivado do Menu até ter mais modelagens 3D e renders relevantes
--- menu.js
+++ menu.js
@@ -4,7 +4,6 @@ class MenuList extends HTMLElement {
             <ul class="cabecalho__menu__lista">
                 <li><a class="cabecalho__menu__link" href="index.html">Home</a></li>
                 <li><a class="cabecalho__menu__link" href="about.html">Sobre</a></li>
-                <li><a class="cabecalho__menu__link" href="archviz.html">Archviz</a></li>
                 <li><a class="cabecalho__menu__link" href="games.html">Games</a></li>
                 <li><a class="cabecalho__menu__link" href="web.html">Web</a></li>
                 <li><a class="cabecalho__menu__link" href="design.html">Design</a></li>
